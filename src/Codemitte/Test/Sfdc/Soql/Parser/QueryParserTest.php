cf720b77dd037a5337c3a2655b88a18f27861ba8
Johannes Heinen <johannes.heinen@gmail.com>
2012-03-07 13:02:25 +0100

    Added some unit tests

---
+++ src/Codemitte/Test/Sfdc/Soql/Parser/QueryParserTest.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Copyright (C) 2012 code mitte GmbH - Zeughausstr. 28-38 - 50667 Cologne/Germany
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace Codemitte\Test\Sfdc\Soql\Parser;
+
+use \PHPUnit_Framework_TestCase;
+
+use Codemitte\Sfdc\Soql\Parser\QueryParser;
+
+/**
+ * @author Johannes Heinen <johannes.heinen@code-mitte.de>
+ * @copyright 2012 code mitte GmbH, Cologne, Germany
+ * @package Test
+ * @subpackage Sfdc
+ */
+class QueryParserTest extends PHPUnit_Framework_TestCase
+{
+    public function testParser1()
+    {
+        $parser = new QueryParser();
+
+        $result = $parser->parse('
+          SELECT
+            ID
+          FROM
+            eps_Loyalty__c
+          WHERE
+            name = :eins AND ping IN (:eins, :zwei)
+            AND test = \':testerer_in_\\\'literal\'
+            pong IN (:eins, :zwei, :drei)
+            AND
+              pingpong = :zwei
+            AND
+              test=\'a literal to die for\' AND var=:drei AND lit2=\'literal 2 value\'
+            AND dings IN :vier
+            AND bool = :bool
+            AND datefield = :String.valueOf(\'pingpong\')
+            AND date = Date.today()
+            AND testtokenizer = \'XXX\'
+            AND testtokenizer2 = \'YYY\'
+            AND testtest = :drei
+            AND date < LAST_FISCAL_QUARTER
+            AND date > NEXT_N_FISCAL_YEARS:3
+            AND date < 2011-12-02
+          ORDER BY hanswurst
+          GROUP BY
+            Id',
+
+                array(
+                     'eins' => 0.127,
+                     'zwei' => 'pong\'hansiwurst',
+                     'drei' => true,
+                     'vier' => array('eins', 'zwei', 'drei', 'vier'),
+                     'bool' => true
+                ));
+
+
+        $this->assertEquals("
+          SELECT
+            ID
+          FROM
+            eps_Loyalty__c
+          WHERE
+            name = 0.127 AND ping IN (0.127, 'pong\'hansiwurst')
+            AND test = ':testerer_in_\'literal'
+            pong IN (0.127, 'pong\'hansiwurst', true)
+            AND
+              pingpong = 'pong\'hansiwurst'
+            AND
+              test='a literal to die for' AND var=true AND lit2='literal 2 value'
+            AND dings IN ('eins', 'zwei', 'drei', 'vier')
+            AND bool = true
+            AND datefield = String.valueOf('pingpong')
+            AND date = Date.today()
+            AND testtokenizer = 'XXX'
+            AND testtokenizer2 = 'YYY'
+            AND testtest = true
+            AND date < LAST_FISCAL_QUARTER
+            AND date > NEXT_N_FISCAL_YEARS:3
+            AND date < 2011-12-02
+          ORDER BY hanswurst
+          GROUP BY
+            Id", $result);
+    }
+}
---
+++ src/Codemitte/Test/Sfdc/Soql/Type/TypeFactoryTest.php
@@ -0,0 +1,197 @@
+<?php
+/**
+ * Copyright (C) 2012 code mitte GmbH - Zeughausstr. 28-38 - 50667 Cologne/Germany
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace Codemitte\Test\Sfdc\Soql\Type;
+
+use \PHPUnit_Framework_TestCase;
+
+use Codemitte\Sfdc\Soql\Type\TypeFactory;
+
+/**
+ * @author Johannes Heinen <johannes.heinen@code-mitte.de>
+ * @copyright 2012 code mitte GmbH, Cologne, Germany
+ * @package Test
+ * @subpackage Sfdc
+ */
+class TypeFactoryTest extends PHPUnit_Framework_TestCase
+{
+    public function testString()
+    {
+        $typeFactory = new TypeFactory();
+
+        $string = $typeFactory->create('teststring');
+
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\String', $string);
+
+        $string = $typeFactory->create('1234');
+
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\String', $string);
+
+        $string = $typeFactory->create('1234.123');
+
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\String', $string);
+    }
+
+    public function testNumber()
+    {
+        $typeFactory = new TypeFactory();
+
+        $number = $typeFactory->create(1234);
+
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\Number', $number);
+
+        $number = $typeFactory->create('1234');
+
+        $this->assertNotInstanceOf('Codemitte\Sfdc\Soql\Type\Number', $number);
+
+        $number = $typeFactory->create(1234.123);
+
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\Number', $number);
+
+        $number = $typeFactory->create('1234.123');
+
+        $this->assertNotInstanceOf('Codemitte\Sfdc\Soql\Type\Number', $number);
+    }
+
+    public function testDate()
+    {
+        $typeFactory = new TypeFactory();
+
+        $date = $typeFactory->create('2011-02-13');
+
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\Date', $date);
+
+        $date = $typeFactory->create('20-02-13');
+
+        $this->assertNotInstanceOf('Codemitte\Sfdc\Soql\Type\Date', $date);
+
+        $date = $typeFactory->create('-02-13');
+
+        $this->assertNotInstanceOf('Codemitte\Sfdc\Soql\Type\Date', $date);
+
+        $date = $typeFactory->create('-2012-02-13');
+
+        $this->assertNotInstanceOf('Codemitte\Sfdc\Soql\Type\Date', $date);
+
+        $date = $typeFactory->create('1989-16-52');
+
+        $this->assertNotInstanceOf('Codemitte\Sfdc\Soql\Type\Date', $date);
+    }
+
+    public function testDateTime()
+    {
+        $typeFactory = new TypeFactory();
+
+        $date = $typeFactory->create('2011-02-13T12:32:12Z');
+
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\DateTime', $date);
+
+        $this->assertEquals('2011-02-13T12:32:12+00:00', $date->toSOQL());
+
+        $date = $typeFactory->create('2011-02-13Z12:32:12-07:32');
+
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\DateTime', $date);
+
+        $date = $typeFactory->create('2011-02-13 12:32');
+
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\DateTime', $date);
+
+        $date = $typeFactory->create('-2011-02-13Z12:32:12-07:32');
+
+        $this->assertNotInstanceOf('Codemitte\Sfdc\Soql\Type\DateTime', $date);
+
+        $date = $typeFactory->create('2011-02-78Z12:32:12-07:32');
+
+        $this->assertNotInstanceOf('Codemitte\Sfdc\Soql\Type\DateTime', $date);
+    }
+
+    public function testBoolean()
+    {
+        $typeFactory = new TypeFactory();
+
+        $bool = $typeFactory->create(true);
+
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\Boolean', $bool);
+
+        $this->assertEquals(true, $bool->getPHPValue());
+
+        $bool = $typeFactory->create(false);
+
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\Boolean', $bool);
+
+        $this->assertEquals(false, $bool->getPHPValue());
+    }
+
+    public function testArrayType()
+    {
+        $typeFactory = new TypeFactory();
+
+        $array = $typeFactory->create(array(
+            'This is a string',
+            1234,
+            1234.5,
+            '2011-02-03',
+            '2011-02-03T12:32+06:30',
+            true,
+            false,
+            array(
+                'This is a string',
+                1234,
+                1234.5,
+                '2011-02-03',
+                '2011-02-03T12:32+06:30',
+                true,
+                false
+            )
+        ));
+
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\ArrayType', $array);
+
+        $values = $array->getPHPValue();
+
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\String', $values[0]);
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\Number', $values[1]);
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\Number', $values[2]);
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\Date', $values[3]);
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\DateTime', $values[4]);
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\Boolean', $values[5]);
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\Boolean', $values[6]);
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\ArrayType', $values[7]);
+
+        $values = $values[7]->getPHPValue();
+
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\String', $values[0]);
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\Number', $values[1]);
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\Number', $values[2]);
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\Date', $values[3]);
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\DateTime', $values[4]);
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\Boolean', $values[5]);
+        $this->assertInstanceOf('Codemitte\Sfdc\Soql\Type\Boolean', $values[6]);
+    }
+
+    public function testExpression()
+    {
+        $e = new \Codemitte\Sfdc\Soql\Type\Expression("ping");
+
+        $this->assertEquals('ping', $e->toSOQL());
+    }
+}
